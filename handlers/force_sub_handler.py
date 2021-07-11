e8aade14f302584d7c64588089623064d21d1d47
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-07-11 06:39:37 +0530

    New Update !!!!
    Done !

---
+++ handlers/force_sub_handler.py
@@ -0,0 +1,44 @@
+# (c) @Damantha_Jasinghe
+
+from configs import Config
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+async def handle_force_sub(bot, cmd):
+    invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
+    try:
+        user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.from_user.id)
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=cmd.from_user.id,
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/AnkiSupport_Official).",
+                parse_mode="markdown",
+                disable_web_page_preview=True
+            )
+            return 400
+    except UserNotParticipant:
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                    ],
+                    [
+                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
+                    ]
+                ]
+            ),
+            parse_mode="markdown"
+        )
+        return 400
+    except Exception:
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="Something went Wrong. Contact my [Support Group](https://t.me/AnkiSupport_Official).",
+            parse_mode="markdown",
+            disable_web_page_preview=True
+        )
+        return 400
